a746ecfa77f67c10b3a129960febfe3d82a96fb3
aunshpatel <patelaunsh28@gmail.com>
2023-07-21 15:09:57 -0400

    Add New Palace form creation with CSS

--- controllers/palaceControllers.js
+++ controllers/palaceControllers.js
@@ -1,10 +1,24 @@
 const Palace = require('../models/palaceModel');
 
 async function index(req, res) {
-    const palaces = await Palace.find({});
-    res.render('palaces/index', { title: 'All Palaces/Forts', palaces });
-  }
+    //const palaces = await Palace.find({});
+    //res.render('palaces/index', { title: 'All Palaces/Forts', palaces });
+    res.render('palaces/index', { title: 'All Palaces/Forts' });
+}
+
+//Details of a palace
+// async function show(req, res) {
+//   const palaces = await Palace.findById(req.params.id);
+//   res.render('palaces/showPalaces', { title: 'Palace/Fort Details', palaces});
+// }
+
+function newPalace(req, res) {
+  res.render('palaces/newPalace', { title: 'Add New Palace/Fort', errorMsg: '' });
+}
 
 module.exports = {
-    index
+    index,
+    //show,
+    new: newPalace,
+
 }
--- routes/palaces.js
+++ routes/palaces.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 const palacesCtrl = require('../controllers/palaceControllers');
 
-// GET /movies
+// GET /palaces
 router.get('/', palacesCtrl.index);
+
+// GET /palaces/new
+router.get('/newPalaces', palacesCtrl.new);
+
+// GET /palaces/:id 
+// router.get('/:id', palacesCtrl.show);
 
 module.exports = router;
