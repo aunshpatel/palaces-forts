61df87adf966e3200fb2897e3b817d5e5dad697b
aunshpatel <patelaunsh28@gmail.com>
2023-07-25 22:32:35 -0400

    Add ensuredLoggedIn

--- routes/palaces.js
+++ routes/palaces.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const palacesCtrl = require('../controllers/palaceControllers');
-//const ensureLoggedIn = require('../config/ensureLoggedIn');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // GET /palaces
 router.get('/', palacesCtrl.index);
 
 // GET /palaces/new
-router.get('/newPalaces', palacesCtrl.new);
+router.get('/newPalaces',ensureLoggedIn, palacesCtrl.new);
 
 //  GET /palaces/:id 
 router.get('/:id', palacesCtrl.show);
 
-router.post('/', palacesCtrl.create);
+//router.post('/', palacesCtrl.create);
 
-//router.post('/', ensureLoggedIn, palacesCtrl.create);
-router.post('/', palacesCtrl.create);
+router.post('/', ensureLoggedIn, palacesCtrl.create);
+//router.post('/', palacesCtrl.create);
 
 router.post('/:id/editPalaces', palacesCtrl.edit);
 
