8b090204a0785385d5145f651bf9c055fe4eb905
aunshpatel <patelaunsh28@gmail.com>
2023-07-27 11:18:35 -0400

    Code cleanup

--- controllers/imageControllers.js
+++ controllers/imageControllers.js
@@ -22,14 +22,9 @@ async function create(req, res) {
 }
 
 async function deletePhoto(req, res) {
-    // Note the cool "dot" syntax to query on the property of a subdoc
     const palace = await Palace.findOne({ 'images._id': req.params.id, 'images.user': req.user._id });
-    console.log("palace"+palace);
     if (!palace) return res.redirect('/palaces');
-    // Remove the review using the remove method available on Mongoose arrays
     palace.images.remove(req.params.id);
-    // Save the updated movie doc
     await palace.save();
-    // Redirect back to the movie's show view
     res.redirect(`/palaces/${palace._id}`);
 }
