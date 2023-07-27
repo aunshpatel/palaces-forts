ed01f973171795496aefe4513d5be2e259876ef9
aunshpatel <patelaunsh28@gmail.com>
2023-07-27 11:35:04 -0400

    Code cleanup

--- controllers/palaceControllers.js
+++ controllers/palaceControllers.js
@@ -3,7 +3,6 @@ const Palace = require('../models/palaceModel');
 async function index(req, res) {
     const palaces = await Palace.find({});
     res.render('palaces/index', { title: 'All Palaces/Forts', palaces });
-    //res.render('palaces/index', { title: 'All Palaces/Forts' });
 }
 
 //Details of a palace
